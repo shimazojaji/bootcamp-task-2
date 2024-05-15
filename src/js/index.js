d06a3fb179a25adea5393257662bcda522369c8e
shimazojaji <shima.zojaji@gmail.com>
2024-05-15 10:37:38 +0330

    third commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,138 +1,190 @@
-const showBtn = document.querySelector(".transaction__display");
+const showBtn = document.querySelector(".transaction__loadBtn");
 const transactionSct = document.querySelector(".transaction");
-const transactionHdr = document.querySelector(".transaction__header");
 const transactionItems = document.querySelector(".transaction__items");
-const transactionSearch = document.querySelector(".transaction__search");
-const transactionSubtitles = document.querySelector(".transaction__subtitles");
-const ascSortPrice = document.querySelector(".transaction__price .arrow-down");
-const descSortPrice = document.querySelector(".transaction__price .arrow-up");
-const ascSortDate = document.querySelector(".transaction__date .arrow-down");
-const descSortDate = document.querySelector(".transaction__date .arrow-up");
+const transactionSearch = document.querySelector(".search__container");
+const transactionBody = document.querySelector(".transaction__body");
+const transactionHeader=document.querySelector('.transaction__subtitle');
+const msgResult=document.createElement("div");
+
+const sortPrice = document.querySelector('.transaction__price .chevaron');
+const sortDate = document.querySelector('.transaction__date .chevaron');
+let isAscending = true;
+
+
 
 let allTransactiondata = [];
 let searchInput = [];
 const filters = {
   searchItems: "",
 };
-let newTags = [];
-showBtn.addEventListener("click", async () => {
-  showHeader();
-  await axios
-    .get("http://localhost:3000/transactions")
-    .then((res) => {
-      allTransactiondata = res.data;
 
-      renderTransactions(res.data, filters);
-    })
-    .catch((err) => console.log(err));
-});
 
-async function renderTransactions(_transactions, _filter) {
-  if (_filter.searchItems) {
+
+// Transactions class
+
+class transactions {
+
+  static async renderTransactions(_transactions, _filter) {
+    if (_filter.searchItems) {
+      axios
+        .get(`http://localhost:3000/transactions?refId_like=${_filter.searchItems}`)
+        .then((res) => {
+          this.showTransaction(res.data);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      this.showTransaction(allTransactiondata);
+    }
+  }
+
+
+  static async getTransactions() {
+
+    domUi.showHeader();
     await axios
-      .get("http://localhost:3000/transactions?refId_like", {
-        params: {
-          q: _filter.searchItems,
-        },
-      })
+      .get("http://localhost:3000/transactions")
       .then((res) => {
-        // console.log(res.data);
-        showTransaction(res.data);
+        allTransactiondata = res.data;
+
+        this.renderTransactions(res.data, filters);
       })
       .catch((err) => console.log(err));
-  } else {
-    showTransaction(allTransactiondata);
   }
+
+
+  static showTransaction(_transactions) {
+     console.log(_transactions.length);
+    if (_transactions.length>0) {
+      domUi.showHeader();
+      _transactions.forEach((item, index) => {
+        const transactionItem = document.createElement("div");
+        transactionItem.classList.add("transaction__item");
+        transactionItem.innerHTML = `
+      <span>${item.id}</span>
+      <span class="type" >${item.type} 
+      </span>
+     <span>${item.price}</span> 
+      <span>${item.refId} </span>
+      <span>
+      ${new Date(item.date).toLocaleString("fa", {
+          dateStyle: "short",
+          timeStyle: "medium",
+        })}</span>`;
+
+        transactionItems.appendChild(transactionItem);
+
+        domUi.changeColor();
+      }
+
+
+      );
+    }
+    else {
+      domUi.hideHeader();
+      // this.showTransaction(_transactions);
+    }
+  }
+
+  static searchTransaction() {
+    transactionSearch.addEventListener("input", (e) => {
+      filters.searchItems = e.target.value;
+      transactionItems.innerHTML = "";
+      this.renderTransactions(allTransactiondata, filters);
+    });
+  }
+
+
 }
 
-function showHeader() {
-  transactionItems.innerHTML = "";
-  transactionSearch.style.visibility = "visible";
-  transactionSubtitles.style.visibility = "visible";
-  transactionItems.style.visibility = "visible";
-}
+// DOM class
 
-function showTransaction(_transactions) {
-  _transactions.forEach((item, index) => {
-    const transactionItem = document.createElement("div");
-    transactionItem.classList.add("transaction__item");
-    transactionItem.innerHTML = `
-    <span>${item.id}</span>
-    <span>
-    ${item.type}
-    
-    
-    </span>
-   <span>${item.price}</span> 
-    <span>${item.refId} </span>
-    <span>
-    ${new Date(item.date).toLocaleString("fa", {
-      dateStyle: "short",
-    })}</span>`;
-
-    transactionItems.appendChild(transactionItem);
-    if (item.type.includes("برداشت از حساب"))
-      transactionItem.style.color = "#de9e46";
-  });
-}
-
-function searchTransaction() {
-  transactionSearch.addEventListener("input", (e) => {
-    filters.searchItems = e.target.value;
+class domUi {
+  static showHeader() {
     transactionItems.innerHTML = "";
-    // console.log(filters.searchItems);
-    renderTransactions(allTransactiondata, filters);
-  });
-}
+    transactionSearch.style.visibility = "visible";
+    transactionBody.style.visibility = "visible";
+    transactionItems.style.visibility = "visible";
+    transactionHeader.style.visibility="visible";
+    showBtn.style.visibility = "hidden";
+    msgResult.style.visibility="hidden";
+  }
+  static hideHeader(){
+    transactionItems.innerHTML = "";
+    transactionSearch.style.visibility = "visible";
+    transactionBody.style.visibility = "visible";
+    transactionHeader.style.visibility="hidden";
+    transactionItems.style.visibility = "hidden";
+    showBtn.style.visibility = "hidden";
+   msgResult.style.visibility="visible";
+   msgResult.classList.add("resultMsg");
 
-async function descendPrice() {
-  transactionItems.innerHTML = "";
-  await axios
-    .get("http://localhost:3000/transactions?_sort=price&_order=desc")
-    .then((res) => {
-      showTransaction(res.data);
+    msgResult.innerHTML=`<span>تراکنش یافت نشد</span>`;
+    transactionBody.appendChild(msgResult);
+
+  }
+  static changeColor() {
+    const typeItems = document.querySelectorAll(".type");
+    typeItems.forEach((item) => {
+      if (item.textContent.includes("برداشت از حساب")) {
+        item.classList.add("red");
+      }
+      else if (item.textContent.includes("افزایش اعتبار")) {
+        item.classList.add("green");
+      }
     })
-    .catch((err) => console.log(err));
+  }
+
 }
 
-async function ascendPrice() {
-  transactionItems.innerHTML = "";
+class sorting {
+  static detectTpySort(nameSort) {
+    if (isAscending) {
+      this.sortData({ nameSort: nameSort, typeSort: 'asc' });
+      isAscending = false;
+      if (nameSort === 'price') {
+         sortPrice.classList.add('rotate');
+         sortDate.classList.remove('rotate');
+      }
+       else{
+        sortDate.classList.add('rotate');
+        sortPrice.classList.remove('rotate');
+       }
+    }
+    else {
+      this.sortData({ nameSort: nameSort, typeSort: 'desc' });
+      isAscending = true;
+       sortPrice.classList.remove('rotate') ;
+        sortDate.classList.remove('rotate');
 
-  await axios
-    .get("http://localhost:3000/transactions?_sort=price&_order=asc")
-    .then((res) => {
-      showTransaction(res.data);
-    })
-    .catch((err) => console.log(err));
+    }
+
+
+
+  }
+  static async sortData({ nameSort, typeSort }) {
+    transactionItems.innerHTML = "";
+    await axios
+      .get(`http://localhost:3000/transactions?_sort=${nameSort}&_order=${typeSort}`)
+      .then((res) => {
+        transactions.showTransaction(res.data);
+      })
+      .catch((err) => console.log(err));
+
+
+  }
+
 }
 
-async function descendDate() {
-  transactionItems.innerHTML = "";
+// loading button event
+showBtn.addEventListener("click", () => {
 
-  await axios
-    .get("http://localhost:3000/transactions?_sort=date&_order=desc")
-    .then((res) => {
-      showTransaction(res.data);
-    })
-    .catch((err) => console.log(err));
+  transactions.getTransactions();
 }
+);
 
-async function ascendDate() {
-  transactionItems.innerHTML = "";
 
-  await axios
-    .get("http://localhost:3000/transactions?_sort=date&_order=asc")
-    .then((res) => {
-      showTransaction(res.data);
-    })
-    .catch((err) => console.log(err));
-}
-
+// Document event
 document.addEventListener("DOMContentLoaded", (event) => {
-  searchTransaction();
-  ascSortPrice.addEventListener("click", descendPrice);
-  descSortPrice.addEventListener("click", ascendPrice);
+  transactions.searchTransaction();
 
-  ascSortDate.addEventListener("click", descendDate);
-  descSortDate.addEventListener("click", ascendDate);
 });
